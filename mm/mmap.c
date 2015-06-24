bd34891e32b4d42e95110b5895faf3c752a5a8d8
Piotr Kwapulinski <kwapulinski.piotr@gmail.com>
2015-06-24 16:58:39 -0700

    mm/mmap.c: optimization of do_mmap_pgoff function
    The simple check for zero length memory mapping may be performed
    earlier.  So that in case of zero length memory mapping some unnecessary
    code is not executed at all.  It does not make the code less readable
    and saves some CPU cycles.
    
    Change-Id: I8f39a4b82341e6c458db51c0c2a2036fee477c37
    Signed-off-by: Piotr Kwapulinski <kwapulinski.piotr@gmail.com>
    Acked-by: Michal Hocko <mhocko@suse.cz>
    Acked-by: Rik van Riel <riel@redhat.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    Signed-off-by: anupritaisno1 <www.anuprita804@gmail.com>

--- mm/mmap.c
+++ mm/mmap.c
@@ -1239,6 +1239,9 @@ unsigned long do_mmap_pgoff(struct file *file, unsigned long addr,
 
 	*populate = 0;
 
+	if (!len)
+		return -EINVAL;
+
 	while (file && (file->f_mode & FMODE_NONMAPPABLE))
 		file = file->f_op->get_lower_file(file);
 
@@ -1251,9 +1254,6 @@ unsigned long do_mmap_pgoff(struct file *file, unsigned long addr,
 	if ((prot & PROT_READ) && (current->personality & READ_IMPLIES_EXEC))
 		if (!(file && (file->f_path.mnt->mnt_flags & MNT_NOEXEC)))
 			prot |= PROT_EXEC;
-
-	if (!len)
-		return -EINVAL;
 
 	if (!(flags & MAP_FIXED))
 		addr = round_hint_to_min(addr);
