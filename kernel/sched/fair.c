646fcdae1eb9705a97ed0ac1a38172e944d526fe
Srinath Sridharan <srinathsr@google.com>
2016-07-14 09:57:29 +0100

    sched: EAS: take cstate into account when selecting idle core
    Introduce a new sysctl for this option, 'sched_cstate_aware'.
    When this is enabled, select_idle_sibling in CFS is modified to
    choose the idle CPU in the sibling group which has the lowest
    idle state index - idle state indexes are assumed to increase
    as sleep depth and hence wakeup latency increase. In this way,
    we attempt to minimise wakeup latency when an idle CPU is
    required.
    
    Signed-off-by: Srinath Sridharan <srinathsr@google.com>
    
    Includes:
    sched: EAS: fix select_idle_sibling
    
    when sysctl_sched_cstate_aware is enabled, best_idle cpu will not be chosen
    in the original flow because it will goto done directly
    
    Bug: 30107557
    Change-Id: Ie09c2e3960cafbb976f8d472747faefab3b4d6ac
    Signed-off-by: martin_liu <martin_liu@htc.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -55,10 +55,8 @@ unsigned int __read_mostly sysctl_sched_wake_to_idle = 0;
  */
 unsigned int sysctl_sched_latency = 6000000ULL;
 unsigned int normalized_sysctl_sched_latency = 6000000ULL;
-
 unsigned int sysctl_sched_is_big_little = 0;
 unsigned int sysctl_sched_sync_hint_enable = 1;
-
 unsigned int sysctl_sched_cstate_aware = 1;
 /*
  * The initial- and re-scaling of tunables is configurable
@@ -5432,9 +5430,9 @@ static int select_idle_sibling(struct task_struct *p, int target)
 	struct sched_domain *sd;
 	struct sched_group *sg;
 	int i = task_cpu(p);
-	int best_idle_cpu = -1;
-	int best_idle_cstate = INT_MAX;
-	unsigned long best_idle_capacity = ULONG_MAX;
+	int best_idle = -1;
+	int best_idle_cstate = -1;
+	int best_idle_capacity = INT_MAX;
 
 	if (!sysctl_sched_cstate_aware) {
 		if (idle_cpu(target))
@@ -5460,20 +5458,18 @@ static int select_idle_sibling(struct task_struct *p, int target)
 
 			if (sysctl_sched_cstate_aware) {
 				for_each_cpu_and(i, tsk_cpus_allowed(p), sched_group_cpus(sg)) {
-
-					int idle_idx = idle_get_state_idx(cpu_rq(i));
-					unsigned long new_usage = boosted_task_utilization(p);
+					struct rq *rq = cpu_rq(i);
+					int idle_idx = idle_get_state_idx(rq);
+					unsigned long new_usage = boosted_task_util(p);
 					unsigned long capacity_orig = capacity_orig_of(i);
-
 					if (new_usage > capacity_orig || !idle_cpu(i))
 						goto next;
 
 					if (i == target && new_usage <= capacity_curr_of(target))
 						return target;
 
-					if (idle_idx < best_idle_cstate &&
-					    capacity_orig <= best_idle_capacity) {
-						best_idle_cpu = i;
+					if (best_idle < 0 || (idle_idx < best_idle_cstate && capacity_orig <= best_idle_capacity)) {
+						best_idle = i;
 						best_idle_cstate = idle_idx;
 						best_idle_capacity = capacity_orig;
 					}
@@ -5493,8 +5489,8 @@ next:
 		} while (sg != sd->groups);
 	}
 
-	if (best_idle_cpu >= 0)
-		target = best_idle_cpu;
+	if (best_idle > 0)
+		target = best_idle;
 
 done:
 	return target;
