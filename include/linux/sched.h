ca21fc83c9ecd23390cb31ace4b1a73e4a18ebf5
Steve Muckle <smuckle@linaro.org>
2016-11-11 14:04:43 -0800

    sched: backport cpufreq hooks from 4.9-rc4
    The scheduler cpufreq hooks are required by the schedutil cpufreq
    governor.
    
    Change-Id: Ied6c46262bb33b7e81bbb3d3d2761124e0c676b7
    Signed-off-by: Steve Muckle <smuckle@linaro.org>
    [trivial cherry-picking fixes]
    Signed-off-by: Juri Lelli <juri.lelli@arm.com>
    Signed-off-by: Chris Redpath <chris.redpath@arm.com>
    Signed-off-by: Pierre2324 <pdbbasketball@gmail.com>

--- include/linux/sched.h
+++ include/linux/sched.h
@@ -3242,6 +3242,22 @@ extern int default_topapp_boost;
 extern struct cgroup_subsys_state *topapp_css;
 int dynamic_boost_write(struct cgroup_subsys_state *css, int boost);
 #endif /* CONFIG_DYNAMIC_STUNE_BOOST */
+#define SCHED_CPUFREQ_RT        (1U << 0)
+#define SCHED_CPUFREQ_DL        (1U << 1)
+#define SCHED_CPUFREQ_IOWAIT    (1U << 2)
+
+#define SCHED_CPUFREQ_RT_DL     (SCHED_CPUFREQ_RT | SCHED_CPUFREQ_DL)
+
+#ifdef CONFIG_CPU_FREQ
+struct update_util_data {
+	void (*func)(struct update_util_data *data, u64 time, unsigned int flags);
+};
+
+void cpufreq_add_update_util_hook(int cpu, struct update_util_data *data,
+                       void (*func)(struct update_util_data *data, u64 time,
+                                    unsigned int flags));
+void cpufreq_remove_update_util_hook(int cpu);
+#endif /* CONFIG_CPU_FREQ */
 
 #endif
 
