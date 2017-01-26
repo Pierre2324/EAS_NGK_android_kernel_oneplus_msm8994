0de24b3905201e607dc32fceb85dc6068b0b2911
Joonwoo Park <joonwoop@codeaurora.org>
2017-01-25 17:07:19 -0800

    cpufreq: sched: WALT: don't apply capacity margin twice
    With WALT all the scheduler classes' load are accounted in scr->cfs and
    update_cpu_capacity_request() adds capacity margin.  At present, at tick
    path, scheduler also adds capacity margin.  Therefore the margin applied
    twice.
    
    Fix such error by using margin applied cpu utilization only for checking
    whether frequency increase is needed.
    
    Change-Id: Id7d8cc73b2e4eec70b274ca66e09bb0b16bf6f09
    Signed-off-by: Joonwoo Park <joonwoop@codeaurora.org>
    (trivial rebase conflict)
    Signed-off-by: Chris Redpath <chris.redpath@arm.com>

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -2855,7 +2855,8 @@ static void sched_freq_tick_walt(int cpu)
 		return sched_freq_tick_pelt(cpu);
 
 	/*
-	 * Add a margin to the WALT utilization.
+	 * Add a margin to the WALT utilization to check if we will need to
+	 * increase frequency.
 	 * NOTE: WALT tracks a single CPU signal for all the scheduling
 	 * classes, thus this margin is going to be added to the DL class as
 	 * well, which is something we do not do in sched_freq_tick_pelt case.
@@ -2864,8 +2865,7 @@ static void sched_freq_tick_walt(int cpu)
 	 * Here we're adding margin, but we're also adding margin in cpufreq.
 	 * There shouldn't be a double addition.
 	 */
-	cpu_utilization = add_capacity_margin(cpu_utilization);
-	if (cpu_utilization <= capacity_curr)
+	if (add_capacity_margin(cpu_utilization) <= capacity_curr)
 		return;
 
 	/*
