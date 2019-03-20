680cdf0487d292cc15e1af1c2865c8421c6a22e1
Pierre2324 <pdbbasketball@gmail.com>
2019-03-20 16:30:17 -0400

    compiler: add compile script
    Signed-off-by: Pierre2324 <pdbbasketball@gmail.com>

---
+++ compile.sh
@@ -0,0 +1,12 @@
+git clone https://bitbucket.org/anupritaisno1/aarch64-linux-gnu -b linaro --single-branch --depth=1 -4 aarch64-linux-gnu -j$(nproc --all);
+export CROSS_COMPILE="/usr/bin/ccache ./aarch64-linux-gnu/bin/aarch64-linux-gnu-" ;
+export ARCH=arm64;
+export SUBARCH=arm64;
+export KBUILD_BUILD_USER="Pierre2324";
+export KBUILD_BUILD_HOST="Pierre-Ubuntu";
+STRIP="aarch64-linux-gnu/bin/aarch64-linux-gnu-strip";
+export CCOMPILE=$CROSS_COMPILE;
+export CROSS_COMPILE="/usr/bin/ccache aarch64-linux-gnu-" ;
+export PATH=$PATH:./aarch64-linux-gnu/bin/ ;
+make nogravity-eas_defconfig;
+make -j$(nproc --all);
